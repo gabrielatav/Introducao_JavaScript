8773e68cc24216541f33f8a3d8dfeef773e566d4
gabrielatav <99209403+gabrielatav@users.noreply.github.com>
2022-09-12 23:13:40 -0300

    Arquivos .js importantes

---
+++ java/cadastroadm.js
@@ -0,0 +1,7 @@
+var cadastro = { nome: "Gabriela", senha:123, email: "gabitav0707@gmail.com", nascimento: "07/07/2005"}
+
+document.getElementsByTagName("body")[0].style.background = "#FADADA"
+document.getElementsByTagName("body")[0].style.color = "black"
+document.getElementsByTagName("label")[2].innerHTML = "Senha"
+document.getElementsByTagName("input")[7].style.background = "#9E3333"
+document.getElementsByTagName("input")[7].style.color = "white"
---
+++ java/produto.js
@@ -0,0 +1,8 @@
+var produto = { nome: "top", descricao:"vermelho", preco:50.00}
+
+document.getElementsByTagName("body")[0].style.background = "#FADADA"
+document.getElementsByTagName("body")[0].style.color = "black"
+document.getElementsByTagName("label")[1].innerHTML = "Descrição do produto"
+document.getElementsByTagName("input")[4].style.background = "#9E3333"
+document.getElementsByTagName("input")[4].style.color = "white"
+document.getElementsByTagName("input")[4].innerHTML = "Selecione a imagem"
